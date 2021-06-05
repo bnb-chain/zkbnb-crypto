71095a955bae0a3434e582b15e15ff2f7c09b222
SherLzp <lzp.sher@gmail.com>
2021-06-05 22:36:43 +0800

    [Sher] add errors

--- zecrey/twistededwards/tebn254/zecrey/errors.go
+++ zecrey/twistededwards/tebn254/zecrey/errors.go
@@ -1,1 +1,11 @@
 package zecrey
+
+import "errors"
+
+var (
+	ErrInvalidParams         = errors.New("err: invalid params")
+	ErrStatements            = errors.New("err: invalid statements")
+	ErrInconsistentPublicKey = errors.New("err: inconsistent public key")
+	ErrInsufficientBalance   = errors.New("err: insufficient balance")
+	ErrInvalidDelta          = errors.New("err: you cannot transfer to yourself positive amount")
+)
