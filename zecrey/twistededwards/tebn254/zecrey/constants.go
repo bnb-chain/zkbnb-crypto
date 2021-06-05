c0872205bf3cb16ca04bc01a539faf694c327e6e
SherLzp <lzp.sher@gmail.com>
2021-06-05 22:36:18 +0800

    [Sher] define constants

--- zecrey/twistededwards/tebn254/zecrey/constants.go
+++ zecrey/twistededwards/tebn254/zecrey/constants.go
@@ -1,1 +1,22 @@
 package zecrey
+
+import (
+	"math/big"
+	curve "zecrey-crypto/ecc/ztwistededwards/tebn254"
+	"zecrey-crypto/elgamal/twistededwards/tebn254/twistedElgamal"
+	"zecrey-crypto/rangeProofs/twistededwards/tebn254/bulletProofs"
+)
+
+type (
+	ElGamalEnc     = twistedElgamal.ElGamalEnc
+	Point          = curve.Point
+	BPSetupParams  = bulletProofs.BPSetupParams
+	AggBulletProof = bulletProofs.AggBulletProof
+)
+
+var (
+	G     = curve.G
+	H     = curve.H
+	Order = curve.Order
+	Zero  = big.NewInt(0)
+)
