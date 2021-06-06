7fc6f95127abb51bcdca8f6cc73c3f7d1f0ff7c0
SherLzp <lzp.sher@gmail.com>
2021-06-06 15:29:53 +0800

    [Sher] move Setup to zecrey.go

--- zecrey/twistededwards/tebn254/zecrey/transferTypes.go
+++ zecrey/twistededwards/tebn254/zecrey/transferTypes.go
@@ -6,19 +6,10 @@ import (
 	curve "zecrey-crypto/ecc/ztwistededwards/tebn254"
 	"zecrey-crypto/elgamal/twistededwards/tebn254/twistedElgamal"
 	"zecrey-crypto/ffmath"
-	"zecrey-crypto/rangeProofs/twistededwards/tebn254/bulletProofs"
 )
 
 type ZSetupParams struct {
 	*BPSetupParams
-}
-
-func Setup(N, M int64) (*ZSetupParams, error) {
-	bpSetupParams, err := bulletProofs.Setup(N, M)
-	if err != nil {
-		return nil, err
-	}
-	return &ZSetupParams{bpSetupParams}, nil
 }
 
 type PTransferProof struct {
--- zecrey/twistededwards/tebn254/zecrey/zecrey.go
+++ zecrey/twistededwards/tebn254/zecrey/zecrey.go
@@ -1,1 +1,11 @@
 package zecrey
+
+import "zecrey-crypto/rangeProofs/twistededwards/tebn254/bulletProofs"
+
+func Setup(N, M int64) (*ZSetupParams, error) {
+	bpSetupParams, err := bulletProofs.Setup(N, M)
+	if err != nil {
+		return nil, err
+	}
+	return &ZSetupParams{bpSetupParams}, nil
+}
