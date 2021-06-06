294347f1b7648bfc8d395c670b7aa6b24543bfbd
SherLzp <lzp.sher@gmail.com>
2021-06-06 17:06:02 +0800

    [Sher] optimize transfer proof

--- zecrey/twistededwards/tebn254/zecrey/transfer.go
+++ zecrey/twistededwards/tebn254/zecrey/transfer.go
@@ -179,10 +179,8 @@ func ProvePTransfer(relation *PTransferProofRelation, params *ZSetupParams) (pro
 			proof.SubProofs[i].z_sk = z_sk
 			proof.SubProofs[i].z_skInv = z_skInv
 			// commit to Pt = Ht^{sk}
-			alpha_zsk := curve.RandomValue()
-			A_Pt := curve.ScalarMul(relation.Ht, alpha_zsk)
+			A_Pt, z_tsk := provePt(nil, statement.Sk, relation.Ht, c)
 			proof.A_Pts = append(proof.A_Pts, A_Pt)
-			z_tsk := ffmath.Add(alpha_zsk, ffmath.Multiply(c, statement.Sk))
 			proof.Z_tsks = append(proof.Z_tsks, z_tsk)
 		}
 		// complete sub proofs
@@ -309,48 +307,6 @@ func (proof *PTransferProof) Verify() (bool, error) {
 	// verify sum proof
 	rSum := proof.A_sum
 	return lSum.Equal(rSum), nil
-}
-
-/**
-commit phase for R_{ValidEnc} = {C_L = pk^r \wedge C_R = g^r h^{b}}
-@pk: public key
-@g: generator
-@h: generator
-*/
-func commitValidEnc(pk, g, h *Point) (
-	alpha_r, alpha_bDelta *big.Int, A_CLDelta, A_CRDelta *Point,
-) {
-	alpha_r = curve.RandomValue()
-	alpha_bDelta = curve.RandomValue()
-	A_CLDelta = curve.ScalarMul(pk, alpha_r)
-	A_CRDelta = curve.Add(curve.ScalarMul(g, alpha_r), curve.ScalarMul(h, alpha_bDelta))
-	return
-}
-
-func respondValidEnc(r, bDelta, alpha_r, alpha_bDelta, c *big.Int) (z_r, z_bDelta *big.Int) {
-	z_r = ffmath.Add(alpha_r, ffmath.Multiply(c, r))
-	z_bDelta = ffmath.Add(alpha_bDelta, ffmath.Multiply(c, bDelta))
-	return
-}
-
-func verifyValidEnc(
-	pk, C_LDelta, A_CLDelta, g, h, C_RDelta, A_CRDelta *Point,
-	c *big.Int,
-	z_r, z_bDelta *big.Int,
-) (bool, error) {
-	if pk == nil || C_LDelta == nil || A_CLDelta == nil || g == nil ||
-		h == nil || C_RDelta == nil || A_CRDelta == nil || c == nil ||
-		z_r == nil || z_bDelta == nil {
-		return false, ErrInvalidParams
-	}
-	l1 := curve.ScalarMul(pk, z_r)
-	r1 := curve.Add(A_CLDelta, curve.ScalarMul(C_LDelta, c))
-	if !l1.Equal(r1) {
-		return false, nil
-	}
-	l2 := curve.Add(curve.ScalarMul(g, z_r), curve.ScalarMul(h, z_bDelta))
-	r2 := curve.Add(A_CRDelta, curve.ScalarMul(C_RDelta, c))
-	return l2.Equal(r2), nil
 }
 
 /**
