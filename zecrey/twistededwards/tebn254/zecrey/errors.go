b29566c76781280c8a9332b8d8fdd4ec6dd44375
SherLzp <lzp.sher@gmail.com>
2021-06-06 15:27:02 +0800

    [Sher] add errors for transfer proof

--- zecrey/twistededwards/tebn254/zecrey/errors.go
+++ zecrey/twistededwards/tebn254/zecrey/errors.go
@@ -5,6 +5,8 @@ import "errors"
 var (
 	ErrInvalidParams         = errors.New("err: invalid params")
 	ErrStatements            = errors.New("err: invalid statements")
+	ErrInvalidChallenge      = errors.New("err: invalid challenge")
+	ErrInvalidBPParams       = errors.New("err: invalid bulletproof prove params")
 	ErrInconsistentPublicKey = errors.New("err: inconsistent public key")
 	ErrInsufficientBalance   = errors.New("err: insufficient balance")
 	ErrInvalidDelta          = errors.New("err: you cannot transfer to yourself positive amount")
