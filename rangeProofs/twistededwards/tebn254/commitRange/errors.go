496198b07d306a8a762948d3477a93d54145d432
SherLzp <lzp.sher@gmail.com>
2021-06-11 17:14:27 +0800

    [Sher] define errors for comRangeProof

--- rangeProofs/twistededwards/tebn254/commitRange/errors.go
+++ rangeProofs/twistededwards/tebn254/commitRange/errors.go
@@ -1,1 +1,9 @@
 package commitRange
+
+import "errors"
+
+var (
+	ErrInvalidRangeParams      = errors.New("err: invalid params for range proof")
+	errInvalidBinaryParams     = errors.New("err: invalid binary params")
+	errInvalidCommitmentParams = errors.New("err: invalid commitment params")
+)
