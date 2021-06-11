bf32d47eac5d3b2aa8e535aa8e442c390b00137a
SherLzp <lzp.sher@gmail.com>
2021-06-11 22:34:11 +0800

    [Sher] add errors

--- zecrey/circuit/bn254/std/errors.go
+++ zecrey/circuit/bn254/std/errors.go
@@ -4,4 +4,5 @@ import "errors"
 
 var (
 	ErrInvalidSetParams = errors.New("err: invalid params to generate circuit")
+	ErrInvalidRangeParams      = errors.New("err: invalid params for range proof")
 )
