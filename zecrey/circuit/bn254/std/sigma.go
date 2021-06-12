8659a6dd358b94bd67966af612f589289d128100
SherLzp <lzp.sher@gmail.com>
2021-06-12 19:29:49 +0800

    [Sher] add examples for Sigma Protocol

--- zecrey/circuit/bn254/std/sigma.go
+++ zecrey/circuit/bn254/std/sigma.go
@@ -6,30 +6,26 @@ import (
 	"github.com/consensys/gnark/std/algebra/twistededwards"
 )
 
-type SchnorrProofCircuit struct {
-	G  Point
+type SchnorrProofConstraints struct {
 	A  Point
 	Pk Point
 	Z  Variable
 	C  Variable
 }
 
-func (circuit *SchnorrProofCircuit) Define(curveID ecc.ID, cs *frontend.ConstraintSystem) error {
+func (circuit *SchnorrProofConstraints) Define(curveID ecc.ID, cs *frontend.ConstraintSystem) error {
 	// get edwards curve params
 	params, err := twistededwards.NewEdCurve(curveID)
 	if err != nil {
 		return err
 	}
 
-	l := Point{
-		X: cs.Constant(circuit.G.X),
-		Y: cs.Constant(circuit.G.Y),
-	}
+	var l, r Point
 
 	l.ScalarMulFixedBase(cs, params.BaseX, params.BaseY, circuit.Z, params)
 
-	pkc := circuit.G.ScalarMulNonFixedBase(cs, &circuit.Pk, circuit.C, params)
-	r := circuit.A.AddGeneric(cs, &circuit.A, pkc, params)
+	r.ScalarMulNonFixedBase(cs, &circuit.Pk, circuit.C, params)
+	r.AddGeneric(cs, &circuit.A, &r, params)
 
 	cs.AssertIsEqual(l.X, r.X)
 	cs.AssertIsEqual(l.Y, r.Y)
@@ -37,7 +33,7 @@ func (circuit *SchnorrProofCircuit) Define(curveID ecc.ID, cs *frontend.Constrai
 	return nil
 }
 
-type OwnershipCircuit struct {
+type OwnershipConstraints struct {
 	G Point
 	YDivT, A_YDivT, H, T,
 	A_T, Pk, A_pk, CLprimeInv,
@@ -46,38 +42,45 @@ type OwnershipCircuit struct {
 	Z_rstarSubrbar, Z_rbar, Z_bprime, Z_sk, Z_skInv Variable
 }
 
-func (circuit *OwnershipCircuit) Define(curveID ecc.ID, cs *frontend.ConstraintSystem) error {
+func (circuit *OwnershipConstraints) Define(curveID ecc.ID, cs *frontend.ConstraintSystem) error {
 	// get edwards curve params
 	params, err := twistededwards.NewEdCurve(curveID)
 	if err != nil {
 		return err
 	}
 
+	var l1, r1 Point
 	// verify Y/T = g^{r^{\star} - \bar{r}}
-	l1 := circuit.G.ScalarMulFixedBase(cs, params.BaseX, params.BaseY, circuit.Z_rstarSubrbar, params)
-	YDivTc := circuit.G.ScalarMulNonFixedBase(cs, &circuit.YDivT, circuit.C, params)
-	r1 := circuit.G.AddGeneric(cs, &circuit.A_YDivT, YDivTc, params)
+	l1.ScalarMulFixedBase(cs, params.BaseX, params.BaseY, circuit.Z_rstarSubrbar, params)
+	r1.ScalarMulNonFixedBase(cs, &circuit.YDivT, circuit.C, params)
+	r1.AddGeneric(cs, &circuit.A_YDivT, &r1, params)
 	cs.AssertIsEqual(l1.X, r1.X)
 	cs.AssertIsEqual(l1.Y, r1.Y)
-	// verify T = g^{\bar{r}} H^{b'}
-	gzrbar := circuit.G.ScalarMulFixedBase(cs, params.BaseX, params.BaseY, circuit.Z_rbar, params)
-	hzbprime := circuit.G.ScalarMulNonFixedBase(cs, &circuit.H, circuit.Z_bprime, params)
-	l2 := circuit.G.AddGeneric(cs, gzrbar, hzbprime, params)
-	Tc := circuit.G.ScalarMulNonFixedBase(cs, &circuit.T, circuit.C, params)
-	r2 := circuit.G.AddGeneric(cs, &circuit.A_T, Tc, params)
+
+	var gzrbar, l2, r2 Point
+	// verify T = g^{\bar{r}} Waste^{b'}
+	gzrbar.ScalarMulFixedBase(cs, params.BaseX, params.BaseY, circuit.Z_rbar, params)
+	l2.ScalarMulNonFixedBase(cs, &circuit.H, circuit.Z_bprime, params)
+	l2.AddGeneric(cs, &gzrbar, &l2, params)
+	r2.ScalarMulNonFixedBase(cs, &circuit.T, circuit.C, params)
+	r2.AddGeneric(cs, &circuit.A_T, &r2, params)
 	cs.AssertIsEqual(l2.X, r2.X)
 	cs.AssertIsEqual(l2.Y, r2.Y)
+
+	var l3, r3 Point
 	// verify Pk = g^{sk}
-	l3 := circuit.G.ScalarMulFixedBase(cs, params.BaseX, params.BaseY, circuit.Z_sk, params)
-	pkc := circuit.G.ScalarMulNonFixedBase(cs, &circuit.Pk, circuit.C, params)
-	r3 := circuit.G.AddGeneric(cs, &circuit.A_pk, pkc, params)
+	l3.ScalarMulFixedBase(cs, params.BaseX, params.BaseY, circuit.Z_sk, params)
+	r3.ScalarMulNonFixedBase(cs, &circuit.Pk, circuit.C, params)
+	r3.AddGeneric(cs, &circuit.A_pk, &r3, params)
 	cs.AssertIsEqual(l3.X, r3.X)
 	cs.AssertIsEqual(l3.Y, r3.Y)
+
+	var l4, r4 Point
 	// verify T(C'_R)^{-1} = (C'_L)^{-sk^{-1}} g^{\bar{r}}
-	CLprimeInv_zskInv := circuit.G.ScalarMulNonFixedBase(cs, &circuit.CLprimeInv, circuit.Z_skInv, params)
-	l4 := circuit.G.AddGeneric(cs, gzrbar, CLprimeInv_zskInv, params)
-	TCRprimeInv_c := circuit.G.ScalarMulNonFixedBase(cs, &circuit.TCRprimeInv, circuit.C, params)
-	r4 := circuit.G.AddGeneric(cs, &circuit.A_TCRprimeInv, TCRprimeInv_c, params)
+	l4.ScalarMulNonFixedBase(cs, &circuit.CLprimeInv, circuit.Z_skInv, params)
+	l4.AddGeneric(cs, &gzrbar, &l4, params)
+	r4.ScalarMulNonFixedBase(cs, &circuit.TCRprimeInv, circuit.C, params)
+	r4.AddGeneric(cs, &circuit.A_TCRprimeInv, &r4, params)
 	cs.AssertIsEqual(l4.X, r4.X)
 	cs.AssertIsEqual(l4.Y, r4.Y)
 	return nil
