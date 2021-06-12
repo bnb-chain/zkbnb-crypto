f1d0e506318c1db5feb7545d643f59b4d02b2f56
SherLzp <lzp.sher@gmail.com>
2021-06-12 19:33:42 +0800

    [Sher] add errors for invalid proof

--- zecrey/circuit/bn254/std/errors.go
+++ zecrey/circuit/bn254/std/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrInvalidSetParams   = errors.New("err: invalid params to generate circuit")
 	ErrInvalidRangeParams = errors.New("err: invalid params for range proof")
 	ErrInvalidChallenge   = errors.New("err: invalid challenge")
+	ErrInvalidProof       = errors.New("err: invalid proof")
 )
