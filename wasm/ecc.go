18130064d82ce94bd4e822dc1f2c14e6c11e7486
SherLzp <lzp.sher@gmail.com>
2021-06-22 19:40:36 +0800

    [Sher] add feature: getL2PublicKey in wasm

--- wasm/ecc.go
+++ wasm/ecc.go
@@ -1,1 +1,27 @@
 package wasm
+
+import (
+	"math/big"
+	"syscall/js"
+	curve "zecrey-crypto/ecc/ztwistededwards/tebn254"
+)
+
+/*
+	GetL2PublicKey: help the user generates the public key
+*/
+func GetL2PublicKey() js.Func {
+	getL2PublicKeyFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) != 1 {
+			return ErrL2SkParams
+		}
+		skStr := args[0].String()
+		sk, b := new(big.Int).SetString(skStr, 10)
+		if !b {
+			return ErrL2SkParams
+		}
+		// pk = g^{sk}
+		pk := curve.ScalarBaseMul(sk)
+		return curve.ToString(pk)
+	})
+	return getL2PublicKeyFunc
+}
