bdaa3203cb0d27c92d14b785ecb26961fbf0c365
SherLzp <lzp.sher@gmail.com>
2021-06-22 17:08:14 +0800

    [Sher] define errors for zecrey wasm

--- wasm/errors.go
+++ wasm/errors.go
@@ -1,1 +1,18 @@
 package wasm
+
+const (
+	ErrUnmarshal                     = -1001
+	ErrInvalidWithdrawParams         = -1002
+	ErrParseEnc                      = -1003
+	ErrParsePoint                    = -1004
+	ErrParseBigInt                   = -1005
+	ErrInvalidWithdrawRelationParams = -1006
+	ErrProveWithdraw                 = -1007
+	ErrMarshalTx                     = -1008
+
+	ErrInvalidTransferParams         = -1009
+	ErrInvalidTransferRelationParams = -1010
+	ErrProveTransfer                 = -1011
+
+	Success = -1
+)
