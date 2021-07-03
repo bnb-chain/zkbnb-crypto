ba6816083cad0ca8790762e8018dd80c04f4a87a
SherLzp <lzp.sher@gmail.com>
2021-07-03 22:49:52 +0800

    [Sher] optimize range proof circuit

--- zecrey/circuit/bn254/std/comRangeProof_test.go
+++ zecrey/circuit/bn254/std/comRangeProof_test.go
@@ -39,12 +39,18 @@ func TestComRangeProofCircuit_Success(t *testing.T) {
 		t.Fatal(err)
 	}
 	for i := 0; i < 1; i++ {
-		b := big.NewInt(3)
-		r := curve.RandomValue()
+		b := big.NewInt(-1)
+		var rs [RangeMaxBits]*big.Int
+		r := big.NewInt(0)
+		for i := 0; i < RangeMaxBits; i++ {
+			rs[i] = curve.RandomValue()
+			r.Add(r, rs[i])
+		}
+		r.Mod(r, curve.Order)
 		g := curve.H
 		h := curve.G
 		T, _ := pedersen.Commit(b, r, g, h)
-		proof, err := commitRange.Prove(b, r, T, g, h, 32)
+		proof, err := commitRange.Prove(b, r, T, rs, g, h)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -62,38 +68,4 @@ func TestComRangeProofCircuit_Success(t *testing.T) {
 
 		assert.SolvingSucceeded(r1cs, &witness)
 	}
-}
-
-func TestComRangeProofCircuit_Failure(t *testing.T) {
-	assert := groth16.NewAssert(t)
-
-	var circuit, witness ComRangeProofConstraints
-	r1cs, err := frontend.Compile(ecc.BN254, backend.GROTH16, &circuit)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	b := big.NewInt(-5)
-	r := curve.RandomValue()
-	g := curve.H
-	h := curve.G
-	T, _ := pedersen.Commit(b, r, g, h)
-	proof, err := commitRange.Prove(b, r, g, h, T, 32)
-	if err != nil {
-		t.Fatal(err)
-	}
-	verify, err := proof.Verify()
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Println("res:", verify)
-	witness, err = setComRangeProofWitness(proof, true)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	fmt.Println("constraints:", r1cs.GetNbConstraints())
-
-	assert.SolvingSucceeded(r1cs, &witness)
-
 }
