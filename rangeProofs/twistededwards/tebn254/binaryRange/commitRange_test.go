fe3b8caf45d8809da7f4242848d83c5a370bc8cc
SherLzp <lzp.sher@gmail.com>
2021-07-03 22:49:36 +0800

    [Sher] optimize commitment range proofs:from 197 mul to 128 mul

--- rangeProofs/twistededwards/tebn254/binaryRange/commitRange_test.go
+++ rangeProofs/twistededwards/tebn254/binaryRange/commitRange_test.go
@@ -18,6 +18,7 @@
 package commitRange
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"math/big"
@@ -25,24 +26,19 @@ import (
 	"time"
 	"zecrey-crypto/commitment/twistededwards/tebn254/pedersen"
 	curve "zecrey-crypto/ecc/ztwistededwards/tebn254"
-	"zecrey-crypto/ffmath"
+	"zecrey-crypto/hash/bn254/zmimc"
+	"zecrey-crypto/util"
 )
 
 func TestProveAndVerify(t *testing.T) {
 	for i := 0; i < 1; i++ {
-		b := big.NewInt(8)
-		var rs [RangeMaxBits]*big.Int
-		sum := big.NewInt(0)
-		for i := 0; i < RangeMaxBits; i++ {
-			rs[i] = curve.RandomValue()
-			sum.Add(sum, rs[i])
-		}
-		r := ffmath.Mod(sum, Order)
+		b := big.NewInt(3)
+		r := curve.RandomValue()
 		g := curve.H
 		h := curve.G
 		T, _ := pedersen.Commit(b, r, g, h)
 		elapse := time.Now()
-		proof, err := Prove(b, r, T, rs, g, h)
+		proof, err := Prove(b, r, T, g, h, 32)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -53,4 +49,24 @@ func TestProveAndVerify(t *testing.T) {
 		}
 		assert.Equal(t, res, true, "ComRangeProof works correctly")
 	}
+}
+
+func TestProveCommitmentSameValue(t *testing.T) {
+	b := big.NewInt(5)
+	r1 := curve.RandomValue()
+	r2 := curve.RandomValue()
+	g := curve.H
+	h := curve.G
+	T, _ := pedersen.Commit(b, r1, g, h)
+	Tprime, _ := pedersen.Commit(b, r2, g, h)
+	A_T, A_Tprime, alpha_b, alpha_r, alpha_rprime, _ := commitCommitmentSameValue(g, h)
+	var buf bytes.Buffer
+	buf.Write(g.Marshal())
+	buf.Write(h.Marshal())
+	buf.Write(A_T.Marshal())
+	buf.Write(A_Tprime.Marshal())
+	c, _ := util.HashToInt(buf, zmimc.Hmimc)
+	zb, zr, zrprime, _ := respondCommitmentSameValue(b, r1, r2, alpha_b, alpha_r, alpha_rprime, c)
+	res, _ := verifyCommitmentSameValue(A_T, A_Tprime, T, Tprime, g, h, zb, zr, zrprime, c)
+	fmt.Println(res)
 }
