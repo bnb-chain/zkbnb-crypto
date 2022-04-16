19d1d9b0c6831456b2c3484d187cfcdf920c25ce
SherLzp <lzp.sher@gmail.com>
2022-04-16 12:29:42 +0800

    [Sher] remove liquidity wasm

--- wasm/zecrey-legend/removeLiquidity.go
+++ wasm/zecrey-legend/removeLiquidity.go
@@ -16,3 +16,37 @@
  */
 
 package zecrey_legend
+
+import (
+	"encoding/json"
+	curve "github.com/zecrey-labs/zecrey-crypto/ecc/ztwistededwards/tebn254"
+	"log"
+	"syscall/js"
+)
+
+func RemoveLiquidityTx() js.Func {
+	helperFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) != 2 {
+			return "invalid swap params"
+		}
+		seed := args[0].String()
+		segmentStr := args[1].String()
+		sk, err := curve.GenerateEddsaPrivateKey(seed)
+		if err != nil {
+			return err.Error()
+		}
+		txInfo, err := ConstructRemoveLiquidityTxInfo(sk, segmentStr)
+		if err != nil {
+			log.Println("[RemoveLiquidityTx] unable to construct generic transfer:", err)
+			return err.Error()
+		}
+		txInfoBytes, err := json.Marshal(txInfo)
+		if err != nil {
+			log.Println("[RemoveLiquidityTx] unable to marshal:", err)
+			return err.Error()
+		}
+		return string(txInfoBytes)
+	})
+	return helperFunc
+}
+
