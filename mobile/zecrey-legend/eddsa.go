90316608e31bd4c3a704b3f02e97c5d6e47933dc
SherLzp <lzp.sher@gmail.com>
2022-06-29 09:48:43 +0800

    [Sher] getEddsaCompressedPublicKey

--- mobile/zecrey-legend/eddsa.go
+++ mobile/zecrey-legend/eddsa.go
@@ -6,21 +6,29 @@ import (
 	curve "github.com/zecrey-labs/zecrey-crypto/ecc/ztwistededwards/tebn254"
 )
 
-func GetEddsaPublicKey(seed string) (pk string) {
+func GetEddsaPublicKey(seed string) (pk string, err error) {
 	sk, err := curve.GenerateEddsaPrivateKey(seed)
 	if err != nil {
-		return err.Error()
+		return "", err
 	}
 	var buf bytes.Buffer
 	buf.Write(sk.PublicKey.A.X.Marshal())
 	buf.Write(sk.PublicKey.A.Y.Marshal())
-	return hex.EncodeToString(buf.Bytes())
+	return hex.EncodeToString(buf.Bytes()), nil
 }
 
-func GenerateEddsaKey(seed string) (skStr string) {
+func GetEddsaCompressedPublicKey(seed string) (pk string, err error) {
 	sk, err := curve.GenerateEddsaPrivateKey(seed)
 	if err != nil {
-		return err.Error()
+		return "", err
 	}
-	return hex.EncodeToString(sk.Bytes())
+	return hex.EncodeToString(sk.PublicKey.Bytes()), nil
+}
+
+func GenerateEddsaKey(seed string) (skStr string, err error) {
+	sk, err := curve.GenerateEddsaPrivateKey(seed)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(sk.Bytes()), nil
 }
