db372c6d8cf0db83a6842034f5073c2c20afa41d
ryan <lightningli0504@gmail.com>
2022-08-04 22:54:08 +0800

    support plonk

--- legend/circuit/bn254/solidity/zkbas_test.go
+++ legend/circuit/bn254/solidity/zkbas_test.go
@@ -22,14 +22,23 @@ import (
 	"github.com/bnb-chain/zkbas-crypto/legend/circuit/bn254/block"
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/backend/plonk"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/frontend/cs/scs"
+	"github.com/consensys/gnark/test"
 	"os"
 	"testing"
+	"text/template"
 )
 
-func TestExportSol(t *testing.T) {
-	differentBlockSizes := []int{1, 10}
+type PlonkProofForDiffBlockSize struct {
+	Vks []plonk.VerifyingKey
+	Sizes []uint16
+}
+
+func TestExportSolGroth16(t *testing.T) {
+	differentBlockSizes := []int{1}
 	for i := 0; i < len(differentBlockSizes); i++ {
 		var circuit block.BlockConstraints
 		circuit.TxsCount = differentBlockSizes[i]
@@ -77,5 +86,73 @@ func TestExportSol(t *testing.T) {
 				panic(err)
 			}
 		}
+	}
+}
+
+func TestExportSolPlonk(t *testing.T) {
+	differentBlockSizes := []uint16{1, 10}
+	var p PlonkProofForDiffBlockSize
+	p.Sizes = differentBlockSizes
+	p.Vks = make([]plonk.VerifyingKey, len(p.Sizes))
+	for i := 0; i < len(differentBlockSizes); i++ {
+		var circuit block.BlockConstraints
+		circuit.TxsCount = int(differentBlockSizes[i])
+		circuit.Txs = make([]block.TxConstraints, circuit.TxsCount)
+		for i := 0; i < circuit.TxsCount; i++ {
+			circuit.Txs[i] = block.GetZeroTxConstraint()
+		}
+		oScs, err := frontend.Compile(ecc.BN254, scs.NewBuilder, &circuit, frontend.IgnoreUnconstrainedInputs())
+		if err != nil {
+			panic(err)
+		}
+		srs, _ := test.NewKZGSRS(oScs)
+		pk, vk, err := plonk.Setup(oScs, srs)
+		if err != nil {
+			panic(err)
+		}
+		{
+			f, err := os.Create("zkbas" + fmt.Sprint(differentBlockSizes[i]) + ".vk_plonk")
+			if err != nil {
+				panic(err)
+			}
+			_, err = vk.WriteTo(f)
+			if err != nil {
+				panic(err)
+			}
+			p.Vks[i] = vk
+		}
+		{
+			f, err := os.Create("zkbas" + fmt.Sprint(differentBlockSizes[i]) + ".pk_plonk")
+			if err != nil {
+				panic(err)
+			}
+			_, err = pk.WriteTo(f)
+			if err != nil {
+				panic(err)
+			}
+		}
+		{
+			f, err := os.Create("zkbas" + fmt.Sprint(differentBlockSizes[i]) + ".srs_plonk")
+			if err != nil {
+				panic(err)
+			}
+			_, err = srs.WriteTo(f)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+	f, err := os.Create("ZkbasPlonkVerifier" + ".sol")
+	if err != nil {
+		panic(err)
+	}
+	tmpl, err := template.New("").Parse(zkbasPlonkVerifierTemplate)
+	if err != nil {
+		panic(err)
+	}
+	// execute template
+	err = tmpl.Execute(f, p)
+	if err != nil {
+		panic(err)
 	}
 }
